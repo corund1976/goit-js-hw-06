583b6646ad6b9bcdef40dc458b4c36a8a6cadcf5
Sergii Budak <corund1976@gmail.com>
2021-09-24 19:21:31 +0300

    Final

--- js/task-08.js
+++ js/task-08.js
@@ -14,12 +14,9 @@ function handleSubmit(event) {
     }
 
     const user = {
-      email: email.value;
-      password: password.value;
+      email: email.value,
+      password: password.value,
     };
-    
-    user.email: email.value;
-    user.password: password.value;
     
     // console.log(`Login: ${email.value}, Password: ${password.value}`);
     console.log(user);  
